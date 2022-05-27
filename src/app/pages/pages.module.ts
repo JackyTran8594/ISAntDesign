77ef9f2882efce422d38915926b68f33743fc4c6
JackyTran <bachtuvu.dev@gmail.com>
2022-05-27 18:37:59 +0700

    update sidebar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -20,7 +20,7 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
     PagesComponent,
-    BreadcrumbComponent
+    BreadcrumbComponent,
   ],
   imports: [
     CommonModule,
